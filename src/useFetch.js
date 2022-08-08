a65c212ccd8e9de7b9e66d391175a9c1e472b84e
Ünal Parlak <unparlak@gmail.com>
2022-08-08 21:58:51 +0300

    feat: new fetch added which gets evolution data

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,38 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export function useFetchData(id) {
-  const [description, setDescription] = useState("");
+// return etmek için array kullan description ve evolvechain diye
 
-  fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
-    .then((res) => res.json())
-    .then((data) => {
-      // to find latest English description of pokemon
-      const flavorText = data.flavor_text_entries;
-      let isFound = false;
-      for (let i = flavorText.length - 1; !isFound; i--) {
-        if (flavorText[i].language.name === "en") {
-          setDescription(flavorText[i].flavor_text);
-          isFound = true;
+export function useFetchExtraData(id) {
+  const [extraData, setExtraData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // to find latest English description of pokemon
+        const flavorText = data.flavor_text_entries;
+        let isFound = false;
+        for (let i = flavorText.length - 1; !isFound; i--) {
+          if (flavorText[i].language.name === "en") {
+            isFound = true;
+            setExtraData((prevState) => ({
+              ...prevState,
+              flavor: flavorText[i].flavor_text,
+            }));
+          }
         }
-      }
 
-      return data.evolution_chain.url;
-    })
-    .then((url) => {
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        });
-    });
+        return data.evolution_chain.url;
+        // e.g. https://pokeapi.co/api/v2/evolution-chain/2 is for charmander, charmeleon and charizard
+      })
+      .then((url) => {
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            let evoChain = [];
+            let evoData = data.chain;
 
-  return description;
+            do {
+              let evoDetails = evoData["evolution_details"][0];
+              let splitUrl = evoData.species.url.split("/");
+
+              evoChain.push({
+                species_name: evoData.species.name,
+                id: splitUrl[splitUrl.length - 2],
+                min_level: !evoDetails ? 1 : evoDetails.min_level,
+                trigger_name: !evoDetails ? null : evoDetails.trigger.name,
+                item: !evoDetails ? null : evoDetails.item,
+              });
+              evoData = evoData["evolves_to"][0];
+            } while (!!evoData && evoData.hasOwnProperty("evolves_to"));
+            setExtraData((prevState) => ({
+              ...prevState,
+              evolution: evoChain,
+            }));
+          });
+      });
+  }, [id]);
+
+  return extraData;
 }
 
-export function useFetchEvolution(id) {
-  const [evolutionChain, setEvolutionChain] = useState("");
+// flavorText[i].flavor_text
 
-  fetch();
-
-  return evolutionChain;
-}
+// setExtraData((prevState) => ({
+//   ...prevState,
+//   evolution: 4,
+// }))
