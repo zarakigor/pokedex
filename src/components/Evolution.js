3e16783891f4247aa555bba5543066adcbfcaed9
Ünal Parlak <unparlak@gmail.com>
2022-08-13 16:23:38 +0300

    map method is added for displaying names and arrows

--- src/components/Evolution.js
+++ src/components/Evolution.js
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
+
 function Evolution(props) {
-  //  props ? console.log(props) : console.log("yok");
-  // conditional ı return un başına koy
+  console.log(props.evolution);
   const evo = props.evolution;
-  return (
-    <div className="pokemon__evolution">
-      {evo && <p> {evo[0].species_name}</p>}
-    </div>
-  );
+  if (evo) {
+    const arr = evo.map((pokemon) => (
+      <div>
+        <div className="arrow"></div>
+        <p>{pokemon.species_name}</p>
+      </div>
+    ));
+    props.evolution.map((name) => console.log(name.species_name));
+    return (
+      <div className="pokemon__evolution">
+        {/* <p> {evo[0].species_name}</p>
+        <div className="arrow"></div> */}
+        {arr}
+      </div>
+    );
+  }
 }
 export default Evolution;
