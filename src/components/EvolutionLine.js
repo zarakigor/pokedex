8674d8078cac3a0c7da6c998bad94bf608c2d9af
Ünal Parlak <unparlak@gmail.com>
2022-08-14 23:14:52 +0300

    name and sprite components are imported

--- src/components/EvolutionLine.js
+++ src/components/EvolutionLine.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FetchSprite } from "../hooks/FetchSprite";
+import Name from "./Name";
+import Sprite from "./Sprite";
 
 function EvolutionLine(props) {
-  if (props) {
-    console.log(props.info);
-  }
+  // if (props) {
+  //   console.log(props.info);
+  // }
 
   return (
     <div className="pokemon__evolution--line">
       <div className="arrow"></div>
-      <img src={FetchSprite(props.info.id)}></img>
-      <p> {props.info.species_name}</p>
+      <Sprite src={FetchSprite(props.info.id)} />
+      <Name name={props.info.species_name} myStyle={{ color: "white" }} />
     </div>
   );
 }
