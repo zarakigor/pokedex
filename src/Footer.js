bcaabb3e3b8a21e148b4691ee50ad39d7a61533a
Ünal Parlak <unparlak@gmail.com>
2022-10-18 22:23:56 +0300

    return button removed

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,9 @@
-import { useNavigate } from "react-router-dom";
-
 function Footer() {
-  const navigate = useNavigate();
   return (
     <div className="footer">
-      <button onClick={() => navigate(-1)} className="btn">
-        GO BACK
-      </button>
       <p>her hakkı saklıdır</p>
     </div>
   );
 }
 
 export default Footer;
-
-// footer ı en altta tut container ,relative, 100vh,root bi şeyler ayarla
